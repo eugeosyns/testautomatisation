0e0d3bd6e66150bf6d5a4dee855d285fd9213a6d
eugeosyns <thomasleo723@gmail.com>
2026-05-06 10:15:24 +0200

    changement visuel

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'augmenter',
         child: const Icon(Icons.add),
       ),
     );
